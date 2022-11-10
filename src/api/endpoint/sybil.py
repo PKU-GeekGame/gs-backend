b9bfc1c6d1b3e7c088297b63a64046ceeab11d28
xmcp <xmcp@pku.edu.cn>
2022-11-11 01:47:40 +0800

    add page state reporting

--- src/api/endpoint/sybil.py
+++ src/api/endpoint/sybil.py
@@ -16,3 +16,12 @@ def recv_sybil_report(req: Request) -> HTTPResponse:
 
     store_anticheat_log(req, ['sybil_report', fp])
     return response.text('OK')
+
+@bp.route('/event', methods=['POST'])
+def recv_sybil_event(req: Request) -> HTTPResponse:
+    event = req.args.get('name')
+    if event not in ['focus', 'blur']:
+        return response.text('bad event', status=404)
+
+    store_anticheat_log(req, ['event', event])
+    return response.text('OK')
