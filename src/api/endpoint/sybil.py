587e72d14f5698283c4e4fa69c7acbebc012f370
xmcp <xmcp@pku.edu.cn>
2023-07-22 23:17:13 +0800

    add api for paste collection

--- src/api/endpoint/sybil.py
+++ src/api/endpoint/sybil.py
@@ -20,8 +20,8 @@ def recv_sybil_report(req: Request) -> HTTPResponse:
 @bp.route('/event', methods=['POST'])
 def recv_sybil_event(req: Request) -> HTTPResponse:
     event = req.args.get('name')
-    if event not in ['focus', 'blur']:
+    if event not in ['focus', 'blur', 'paste']:
         return response.text('bad event', status=404)
 
-    store_anticheat_log(req, ['event', event])
+    store_anticheat_log(req, ['event', event, req.json])
     return response.text('OK')
