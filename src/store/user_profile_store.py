796079bdea65596e118e8027c00119067e16b967
xmcp <xmcp@pku.edu.cn>
2023-07-25 04:32:38 +0800

    improve detection of invalid nicknames

--- src/store/user_profile_store.py
+++ src/store/user_profile_store.py
@@ -50,13 +50,23 @@ class UserProfileStore(Table):
         'banned': ['nickname', 'qq', 'comment'],
     }
 
+    # https://unicode.org/reports/tr51/proposed.html
+    EMOJI_CHARS = (
+        {chr(0x200d)}  # zwj
+        | {chr(0x200b)}  # zwsp, to break emoji componenets into independent chars
+        | {chr(0x20e3)} # keycap
+        | {chr(c) for c in range(0xfe00, 0xfe0f+1)} # variation selector
+        | {chr(c) for c in range(0xe0020, 0xe007f+1)} # tag
+        | {chr(c) for c in range(0x1f1e6, 0x1f1ff+1)} # regional indicator
+    )
+
     # https://www.compart.com/en/unicode/category
     DISALLOWED_CHARS = (
-       unicode_chars('Cc', 'Cf', 'Cs', 'Mc', 'Me', 'Mn', 'Zl', 'Zp')
-       | {chr(c) for c in range(0x12423, 0x12431+1)}
-       | {chr(0x0d78)}
-    ) - {chr(0x200b), chr(0x200c), chr(0x200d)}
-    WHITESPACE_CHARS = unicode_chars('Zs') | {chr(0x200b), chr(0x200c), chr(0x200d)}
+        unicode_chars('Cc', 'Cf', 'Cs', 'Mc', 'Me', 'Mn', 'Zl', 'Zp') # control and modifier chars
+        | {chr(c) for c in range(0x12423, 0x12431+1)} # too long
+        | {chr(0x0d78)} # too long
+    ) - EMOJI_CHARS
+    WHITESPACE_CHARS = unicode_chars('Zs') | EMOJI_CHARS
 
     @classmethod
     def _deep_val_nickname(cls, name: str) -> Optional[str]:
